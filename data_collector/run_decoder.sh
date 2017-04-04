68f3277ac8f1ff40fea14d047024d76087584905
fresheed <ok.john.rus@gmail.com>
2017-04-04 17:02:28 +0300

    added logs decoder to repo

---
+++ data_collector/run_decoder.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+javacommon_jar=/home/fresheed/AndroidStudioProjects/ActionLogger/javacommon/build/libs/javacommon.jar
+
+rm -rf parsed_logs
+mkdir parsed_logs
+
+for log in $(ls -p raw_logs | grep -v /); do 
+    echo "Parsing $log"
+    groovy -cp "$javacommon_jar" decode_logs.groovy raw_logs/$log > parsed_logs/$log.txt
+done
+
+
+
